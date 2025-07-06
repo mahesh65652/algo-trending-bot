3a391a3424df3278dbe2d2b534873ce3e5fb930d
Mahesh ramavat <Mramavat52@gmail.com>
2025-07-06 15:39:26 +0530

    Create telegram_alert.py

---
+++ telegram_alert.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+def send_telegram_message(message):
+    bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
+    chat_id = os.getenv("TELEGRAM_CHAT_ID")
+
+    url = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+    payload = {
+        "chat_id": chat_id,
+        "text": message,
+        "parse_mode": "Markdown"
+    }
+
+    response = requests.post(url, data=payload)
+    return response.json()
