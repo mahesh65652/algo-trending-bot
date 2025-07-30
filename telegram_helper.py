db0b9a33ab6d19b4ce490851f32e16244ec885f1
Mahesh ramavat <Mramavat52@gmail.com>
2025-07-30 17:12:18 +0530

    Create telegram_helper.py

---
+++ telegram_helper.py
@@ -0,0 +1,11 @@
+import os
+import requests
+
+def send_telegram(message):
+    token = os.environ.get("TELEGRAM_BOT_TOKEN")
+    chat_id = os.environ.get("TELEGRAM_CHAT_ID")
+    if token and chat_id:
+        url = f"https://api.telegram.org/bot{token}/sendMessage"
+        requests.post(url, data={"chat_id": chat_id, "text": message})
+    else:
+        print("⚠️ Telegram token or chat_id not found.")
