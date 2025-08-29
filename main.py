7d465718cae080707b8a0111e4907ec29c89a74a
Mahesh ramavat <Mramavat52@gmail.com>
2025-08-29 13:33:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import sys
 from gspread.exceptions import WorksheetNotFound, APIError
 import time
 from datetime import datetime, timedelta
+import re  
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', stream=sys.stdout)
